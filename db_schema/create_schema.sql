a06e0ca947dbcb2990e2f68f383455b6c4363646
Phillip Taylor <phil.taylor@net-a-porter.com>
2011-05-25 16:38:18 +0100

    Rename tables in schema.

--- db_schema/create_schema.sql
+++ db_schema/create_schema.sql
@@ -1,20 +1,66 @@
 
-create table tCategory (
+drop table tagged_clothing;
+drop table clothing;
+drop table category;
+drop table outfit;
+
+create table category (
 	category_id   integer not null,
 	name          varchar(255) not null
 );
 
-alter table tCategory add constraint cCategory_pk1
+alter table category add constraint ccategory_pk1
 	primary key (category_id);
 
-create table tClothing (
+create table clothing (
 	clothing_id   integer not null,
 	name          varchar(255) not null,
 	category_id   integer not null
 );
 
-alter table tClothing add constraint cClothing_pk1
+alter table clothing add constraint cclothing_pk1
 	primary key (clothing_id);
 
-alter table tClothing add constraint cClothing_fk1
+alter table clothing add constraint cclothing_fk1
 	foreign key (category_id) references tCategory (category_id);
+
+create table outfit (
+	outfit_id      integer not null,
+	name           varchar(255) not null
+);
+
+alter table outfit add constraint coutfit_pk
+	primary key(outfit_id);
+
+create table tagged_clothing (
+	clothing_id    integer not null,
+	outfit_id      integer not null
+);
+
+alter table tagged_clothing add constraint ctagged_clothing_pk1
+	primary key (clothing_id, outfit_id);
+
+
+-- insert some sample data
+
+insert into category values (1, 'Shirt');
+insert into category values (2, 'Shoes');
+insert into category values (3, 'Socks');
+insert into category values (4, 'Shorts');
+
+insert into clothing values (1, 'Blue Shirt', 1);
+insert into clothing values (2, 'Red Shirt', 1);
+insert into clothing values (3, 'Green Shirt', 1);
+insert into clothing values (4, 'Yellow Shirt', 1);
+
+insert into clothing values (5, 'Black Shoes', 2);
+insert into clothing values (6, 'Brown Shoes', 2);
+
+insert into clothing values (7, 'Brown Socks', 3);
+insert into clothing values (8, 'White Socks', 3);
+insert into clothing values (9, 'Black Socks', 3);
+
+insert into clothing values (10, 'Swimming Trunks', 4);
+insert into clothing values (11, 'Baggy Shorts', 4);
+insert into clothing values (12, 'Bermuda Shorts', 4);
+insert into clothing values (13, 'Professional Style Shorts', 4);
--- lib/Wardrobe/Model/Main/Result/Category.pm
+++ lib/Wardrobe/Model/Main/Result/Category.pm
@@ -2,7 +2,7 @@
 package Wardrobe::Model::Main::Result::Category;
 use base qw/DBIx::Class::Core/;
 
-__PACKAGE__->table('tCategory');
+__PACKAGE__->table('category');
 __PACKAGE__->add_columns(qw/category_id name/);
 __PACKAGE__->set_primary_key('category_id');
 __PACKAGE__->has_many('clothes', 'Wardrobe::Model::Main::Result::Clothing', 'category_id');
